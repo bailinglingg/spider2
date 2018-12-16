217c1d7e0fc9c3fa0d51aae06f3b163d54575d17
bailinglingg <45841374+bailinglingg@users.noreply.github.com>
2018-12-16 17:21:45 +0800

    解析影片信息

--- src/edu/csuft/bailing/spider2/App.java
+++ src/edu/csuft/bailing/spider2/App.java
@@ -1,5 +1,48 @@
 package edu.csuft.bailing.spider2;
 
-public class App {
+import java.io.IOException;
 
-}
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ *程序的入口 
+ * @author zab12109
+ *
+ */
+public class App {
+//alt+/
+	public static void main(String[] args) {
+		
+	//目标URL
+		String url="http://movie.douban.com/top250";
+		//使用JSOUP抓取文档
+	
+		
+			try {
+				Document doc= Jsoup.connect(url).get();
+				
+				Elements es = doc.select(".grid_view .item");
+				System.out.println(es.size());
+				
+				for(Element e :es) {
+					//每一部影片
+					Element t=e.select(".title").first();
+					String num=e.select(".star span").last().text();
+					System.out.println(t.text()+","+num);
+				}
+				
+				
+//				String title=doc.title();
+//				String data=doc.data();
+				
+//				System.out.println(title);
+//				System.out.println(data);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	}
