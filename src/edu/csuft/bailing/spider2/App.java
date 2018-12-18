228162ffd05c47e6e9ed055e7bff480639a995ee
bailinglingg <45841374+bailinglingg@users.noreply.github.com>
2018-12-18 17:11:37 +0800

    将抓取的数据在电子表格中显示

--- src/edu/csuft/bailing/spider2/App.java
+++ src/edu/csuft/bailing/spider2/App.java
@@ -1,8 +1,12 @@
 package edu.csuft.bailing.spider2;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import javafx.print.Collation;
 
 /**
  *程序的入口 
@@ -18,15 +22,53 @@ public class App {
 		ExecutorService pool= Executors.newFixedThreadPool(4);
 		
 		//无限缓存
-//		pool=Executors.newCachedThreadPool();
+		pool=Executors.newCachedThreadPool();
 		//一个线程
-//		pool=Executors.newSingleThreadExecutor();
+		pool=Executors.newSingleThreadExecutor();
+		
+		
 		ArrayList<Film> list=new ArrayList<>();
-		String url="http://movie.douban.com/top250?start=";
+		
+		
+		String url="http://movie.douban.com/top250?start";
+		
 		for(int i=0;i<10;i++) {
 			String path=String.format("%s=%d",url,i*25);
-		  pool.submit(new Spider(path,list));	
-		  
+		  pool.submit(new Spider(path,list));	 
+		}
+		
+		
+		pool.shutdown();
+				
+		while(!pool.isTerminated()) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		//数据排序
+		System.out.println(list.size());
+		for(Film film:list) {
+			System.out.print(film.toCSV());
+		}
+		//写入文件
+		String file="d:/film.csv";//绝对路径
+		file="film.csv";         //相对路径
+		
+		//io
+		//排序
+		Collections.sort(list);
+		try (FileWriter out=new FileWriter(file)){
+			//写数据
+			for (Film film : list) {
+				out.write(film.toCSV());
+			}
+			System.out.println("ok");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
 	}
 	
--- src/edu/csuft/bailing/spider2/Film.java
+++ src/edu/csuft/bailing/spider2/Film.java
@@ -4,7 +4,7 @@ package edu.csuft.bailing.spider2;
  * @author zab12109
  *
  */
-public class Film {
+public class Film implements Comparable<Film>{
 	
 	/**
 	 * Ó°Æ¬Ãû³Æ
@@ -40,11 +40,25 @@ public class Film {
 	 * ¶ÌÆÀ
 	 */
 	String quote;
+	
+	public String toCSV(){
+		return String.format("%d,%s,%d,%.1f\n",
+				id,
+				title,
+				num,
+				rating);	
+	}
 
 	@Override
 	public String toString() {
 		return "Film [title=" + title + ", info=" + info + ", rating=" + rating + ", num=" + num + ", id=" + id
 				+ ", poster=" + poster + ", quote=" + quote + "]";
+	}
+
+	@Override
+	public int compareTo(Film o) {
+		// TODO Auto-generated method stub
+		return id-o.id;
 	}
 	
 
